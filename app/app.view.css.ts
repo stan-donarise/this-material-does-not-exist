670d4c69a399aa341cfee304e792ba0edea242ce
stan.donarise <stan.donarise@gmail.com>
2024-06-14 14:12:14 +1100

    $mpds_tmdne_app initial

---
+++ app/app.view.css.ts
@@ -0,0 +1,45 @@
+namespace $.$$ {
+	
+	$mol_style_define( $mpds_tmdne_app, {
+
+		padding: {
+			top: $mol_gap.block,
+			bottom: $mol_gap.block,
+		},
+		flex: {
+			direction: 'column',
+		},
+		
+		Player: {
+			flex: {
+				grow: 1,
+			},
+		},
+
+		Head: {
+			justify: {
+				content: 'center',
+			},
+		},
+
+		Name: {
+			justify: {
+				content: 'center',
+			},
+		},
+
+		Question: {
+			justify: {
+				content: 'center',
+			},
+		},
+
+		Answer: {
+			justify: {
+				content: 'center',
+			},
+		},
+		
+	} )
+	
+}
---
+++ app/app.view.ts
@@ -0,0 +1,26 @@
+namespace $.$$ {
+
+	export class $mpds_tmdne_app extends $.$mpds_tmdne_app {
+
+		@ $mol_mem
+		json() {
+			return this.$.$mol_fetch.json( `https://optimade-gnome.odbx.science/v1/structures?page_limit=1&page_offset=${this.number()}` ) as any
+		}
+
+		@ $mol_mem
+		name() {
+			return this.json()?.data[0]?.attributes?.chemical_formula_reduced ?? ''
+		}
+
+		@ $mol_action
+		update() {
+			this.number( this.number( random_int( 1, 384937 ) ) )
+		}
+		
+	}
+
+	function random_int( min: number, max: number ) {
+		return Math.floor( Math.random() * ( max - min + 1 ) ) + min
+	}
+	
+}
