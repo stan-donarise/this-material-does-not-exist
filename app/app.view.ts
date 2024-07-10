e88fa0963e657a67852d04fe5113be34b5ccb1e9
stan.donarise <stan.donarise@gmail.com>
2024-07-10 13:54:30 +0300

    request body instead of query string

--- app/app.view.ts
+++ app/app.view.ts
@@ -14,10 +14,14 @@ namespace $.$$ {
 
 			const structure = JSON.stringify( this.fetch_by_number( number ) )
 			const params = new URLSearchParams({ structure })
-			const url = `https://labs.mpds.io/predict?${ params.toString() }`
+			const url = `https://labs.mpds.io/predict`
 
 			const prediction = this.$.$mol_wire_sync( this ).$.$mol_fetch.success( url, {
 				method: 'post',
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+				body: params.toString(),
 			} ).json() ?? {} as any
 
 			return prediction
